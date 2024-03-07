f2b4d49bee8dc667562b35ab6131570e96b91e03
Hybes <ben.hybert21@gmail.com>
2024-03-07 00:42:14 +0000

    Update window dimensions and enable context isolation

--- main.js
+++ main.js
@@ -31,11 +31,12 @@ ipcMain.on('open-directory-dialog', (event) => {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 400,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true
+      nodeIntegration: false,
+      contextIsolation: true
     }
   })
 
